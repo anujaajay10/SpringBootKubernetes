c3d62760e56e8f99da398e2d8d58d34a4652e117
Anuja Ajay <anujajay10@gmail.com>
2023-06-08 09:14:18 +0530

    first commit

---
+++ SpringBoot-Kubernetes/src/main/java/com/ust/SpringBootKubernetes/SpringBootKubernetesApplication.java
@@ -0,0 +1,22 @@
+package com.ust.SpringBootKubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKubernetesApplication.class, args);
+
+
+}
+@GetMapping("/welcome")
+public String welcome(){
+	return "Welcome!!";
+}
+}
---
+++ SpringBoot-Kubernetes/src/test/java/com/ust/SpringBootKubernetes/SpringBootKubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.SpringBootKubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
